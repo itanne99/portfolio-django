156a38897b3b940ce6ed5740368085fab86f86ec
Ido Tanne <idovader@gmail.com>
2022-01-17 21:34:24 -0500

    setup basic view for Home

--- home/views.py
+++ home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    template = loader.get_template('index.html')
+    context = {
+        'title': "Home",
+    }
+    return HttpResponse(template.render(context, request))
