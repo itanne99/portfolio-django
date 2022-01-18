62a2328cd8d094f0085955e15aab744c92cf6f1f
Ido Tanne <idovader@gmail.com>
2022-01-17 21:53:47 -0500

    update home app to display database info

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Projects(models.Model):
+    name = models.CharField(max_length=256)
+    url = models.CharField(max_length=256)
+    image = models.CharField(max_length=256)
--- home/views.py
+++ home/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Projects
 
 
 def index(request):
+    projects = Projects.objects.order_by('-name')
     template = loader.get_template('index.html')
     context = {
         'title': "Home",
+        'projects': projects,
     }
     return HttpResponse(template.render(context, request))
