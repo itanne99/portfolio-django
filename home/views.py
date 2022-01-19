876609c9ae71d984fd90aad2709a6924c4211a8b
Ido Tanne <idovader@gmail.com>
2022-01-19 11:18:45 -0500

    added database link of skills to front-end visual

--- home/views.py
+++ home/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-from .models import Projects
+from .models import Projects, FrontEndSkills, BackEndSkills, DesignSkills
 
 
 def index(request):
     projects = Projects.objects.order_by('-name')
+    frontEndSkills = FrontEndSkills.objects.order_by('-skill')
+    backEndSkills = BackEndSkills.objects.order_by('-skill')
+    designSkills = DesignSkills.objects.order_by('-skill')
     template = loader.get_template('index.html')
     context = {
         'title': "Home",
         'projects': projects,
+        'frontEndSkills': frontEndSkills,
+        'backEndSkills': backEndSkills,
+        'designSkills': designSkills,
     }
     return HttpResponse(template.render(context, request))
