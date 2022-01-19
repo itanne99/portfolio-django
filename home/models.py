e59eedc2008641d07b877aaa456845c3e45e039f
Ido Tanne <idovader@gmail.com>
2022-01-19 11:15:35 -0500

    added model for different skill sets

--- home/models.py
+++ home/models.py
@@ -7,3 +7,15 @@ class Projects(models.Model):
     name = models.CharField(max_length=256)
     url = models.CharField(max_length=256)
     image = models.CharField(max_length=256)
+
+
+class FrontEndSkills(models.Model):
+    skill = models.CharField(max_length=256)
+
+
+class BackEndSkills(models.Model):
+    skill = models.CharField(max_length=256)
+
+
+class DesignSkills(models.Model):
+    skill = models.CharField(max_length=256)
